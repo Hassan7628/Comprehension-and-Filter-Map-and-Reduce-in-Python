c0495a30205f97a1fe54898516569e0414daa242
Hassan7628 <minthassan7628@gmail.com>
2025-06-03 16:59:04 +0500

    Fix Map Function

--- MapFuction.py
+++ MapFuction.py
@@ -8,15 +8,15 @@ for pkr in convert:
     print(f"${pkr/280} : Rs{pkr}")
 
 # Name Formatter:
-names=["HAssan","AlI","aHMEd","sHUNan","KiNzA"]
+names = ["HAssan", "AlI", "aHMEd", "sHUNan", "KiNzA"]
 
-organized_name=list(map(lambda x:x.title(),names))
+organized_name = list(map(lambda x: x.title(), names))
 print(f"\nOrganized names: {organized_name}\n")
 
 # Square Root Calculator:
-numbers=[11,22,4,5,22,101,33,2]
+numbers = [11, 22, 4, 5, 22, 101, 33, 2]
 
-sqrt=list(map(lambda x: x**0.5,numbers))
+sqrt = list(map(lambda x: x**0.5, numbers))
 
 print("\nSquare Root:\n")
 for sq in sqrt:
